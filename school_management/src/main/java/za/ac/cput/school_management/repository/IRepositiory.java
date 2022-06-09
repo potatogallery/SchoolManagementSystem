897d72e5ec521cc567734de7b18854e179c0f69a
potatogallery <khazsaunders@hotmail.com>
2022-06-09 23:11:03 +0200

    Maven Project
    School_Manangement Maven project created.
    *POM file updated with the required dependencies, and plugins + Springboot Initializer*

---
+++ school_management/src/main/java/za/ac/cput/school_management/repository/IRepositiory.java
@@ -0,0 +1,4 @@
+package za.ac.cput.school_management.repository;
+
+public interface IRepositiory {
+}
---
+++ school_management/src/main/java/za/ac/cput/school_management/service/IService.java
@@ -0,0 +1,4 @@
+package za.ac.cput.school_management.service;
+
+public interface IService {
+}
