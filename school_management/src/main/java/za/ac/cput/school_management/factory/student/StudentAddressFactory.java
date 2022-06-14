711a36ee76827e7652e01a2580b95fb870b5d7c4
Zavian007 <81582695+Zavian007@users.noreply.github.com>
2022-06-15 01:02:20 +0200

    Update StudentAddressFactory.java

--- school_management/src/main/java/za/ac/cput/school_management/factory/student/StudentAddressFactory.java
+++ school_management/src/main/java/za/ac/cput/school_management/factory/student/StudentAddressFactory.java
@@ -1,10 +1,17 @@
 package za.ac.cput.school_management.factory.student;
 
+/*
+    Student.java
+    Entity for the Student
+    Zavian Minnies(217230458)
+    14 June 2022
+ */
 
+import za.ac.cput.school_management.domain.lookup.Address;
 import za.ac.cput.school_management.domain.student.StudentAddress;
 
 
-public class public class StudentAddressFactory {
+public class StudentAddressFactory {
 
     public static StudentAddress createStudentAddress(String studentId, Address address) {
 
