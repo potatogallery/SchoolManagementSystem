38743d58119c4dd99d9f118c8c4b73e4c00bb903
WajiWaj <216287820@mycput.ac.za>
2022-06-15 03:29:18 -0700

    employee

--- school_management/src/main/java/za/ac/cput/school_management/factory/student/StudentAddressFactory.java
+++ school_management/src/main/java/za/ac/cput/school_management/factory/student/StudentAddressFactory.java
@@ -1,4 +1,22 @@
 package za.ac.cput.school_management.factory.student;
 
+<<<<<<< Updated upstream
 public class StudentAddressFactory {
+=======
+
+import za.ac.cput.school_management.domain.lookup.Address;
+import za.ac.cput.school_management.domain.student.StudentAddress;
+
+
+public class StudentAddressFactory {
+
+    public static StudentAddress createStudentAddress(String studentId, Address address) {
+
+        StudentAddress studentAddress = new StudentAddress.Builder()
+                .setStudentId(studentId)
+                .setAddress(address)
+                .build();
+        return studentAddress;
+    }
+>>>>>>> Stashed changes
 }
