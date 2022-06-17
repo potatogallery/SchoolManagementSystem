c705c5eb9c75426cc0c1876f7b3f4cd9086b6be0
RedarBeh <80404526+RedarBeh@users.noreply.github.com>
2022-06-17 12:26:12 +0200

    Service files for city and country completed

---
+++ school_management/src/main/java/za/ac/cput/school_management/service/city/CountryService.java
@@ -0,0 +1,8 @@
+package za.ac.cput.school_management.service.city;
+
+import za.ac.cput.school_management.domain.city.Country;
+import za.ac.cput.school_management.service.IService;
+
+public interface CountryService extends IService<Country, String> {
+
+}
