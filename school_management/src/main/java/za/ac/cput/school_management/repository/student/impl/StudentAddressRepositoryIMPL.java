3087848c08a354b3d05b6e8c3757615d1654deb4
Zavian007 <81582695+Zavian007@users.noreply.github.com>
2022-06-18 16:44:41 +0200

    Update StudentAddressRepositoryIMPL.java

--- school_management/src/main/java/za/ac/cput/school_management/repository/student/impl/StudentAddressRepositoryIMPL.java
+++ school_management/src/main/java/za/ac/cput/school_management/repository/student/impl/StudentAddressRepositoryIMPL.java
@@ -1,4 +1,17 @@
 package za.ac.cput.school_management.repository.student.impl;
 
-public class StudentAddressRepositoryIMPL {
+import za.ac.cput.school_management.domain.student.Student;
+import za.ac.cput.school_management.repository.student.StudentRepository;
+import org.springframework.stereotype.Repository;
+
+
+/*
+Zavian Minnies - 217230458
+ADP3 JUNE ASSIGNMENT
+StudentAddressRepositoryIMPL.java
+*/
+
+
+public abstract class StudentRepositoryIMPL implements StudentRepository {
+
 }
