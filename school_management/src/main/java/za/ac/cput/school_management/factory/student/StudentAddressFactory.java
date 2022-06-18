797bfe5ad4e24566dd0d801f38f2feeb5fc20b12
Zavian007 <81582695+Zavian007@users.noreply.github.com>
2022-06-18 11:54:54 +0200

    Update StudentAddressFactory.java

--- school_management/src/main/java/za/ac/cput/school_management/factory/student/StudentAddressFactory.java
+++ school_management/src/main/java/za/ac/cput/school_management/factory/student/StudentAddressFactory.java
@@ -6,6 +6,12 @@ import org.apache.tomcat.jni.Address;
 import za.ac.cput.school_management.domain.lookup.Address;
 import za.ac.cput.school_management.domain.student.StudentAddress;
 
+/*
+    StudentAddress.java
+    Entity for the StudentAddress
+    Zavian Minnies (217230458)
+    14 June 2022
+ */
 
 public class StudentAddressFactory {
 
