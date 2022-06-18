5ab9cf62d3ecdf94111e125e2171f61970d878d7
Zavian007 <81582695+Zavian007@users.noreply.github.com>
2022-06-18 11:39:24 +0200

    Update StudentAddressRepository.java

--- school_management/src/main/java/za/ac/cput/school_management/repository/student/StudentAddressRepository.java
+++ school_management/src/main/java/za/ac/cput/school_management/repository/student/StudentAddressRepository.java
@@ -1,4 +1,4 @@
 package za.ac.cput.school_management.repository.student;
 
-public interface studentAddressRepository extends IRepository <studentAddress, String> {
+public interface StudentAddressRepository extends IRepository <StudentAddress, String> {
 }
