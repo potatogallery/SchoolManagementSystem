4efbe7a2320aced159d1605fe61f6510f3c33c71
Zavian007 <81582695+Zavian007@users.noreply.github.com>
2022-06-18 11:41:58 +0200

    Update StudentAddressRepository.java

--- school_management/src/main/java/za/ac/cput/school_management/repository/student/StudentAddressRepository.java
+++ school_management/src/main/java/za/ac/cput/school_management/repository/student/StudentAddressRepository.java
@@ -1,4 +1,8 @@
 package za.ac.cput.school_management.repository.student;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.school_management.domain.Lookup.Name;
+import org.springframework.stereotype.Repository;
+
 public interface StudentAddressRepository extends IRepository <StudentAddress, String> {
 }
